264e9565175a580837994f101f6bef4978ecf425
jysophia <jysophiakim@gmail.com>
2025-09-22 13:58:49 -0400

    first commit

---
+++ FlexibleGridLayout.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleGridLayout : LayoutGroup
+{
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+    public override void CalculateLayoutInputVertical()
+    {
+        // TODO: Implement layout calculation logic here
+
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+        // TODO: Implement horizontal layout logic here
+        base.CalculateLayoutInputHorizontal();
+        float sqareRoot = Mathf.Sqrt(transform.childCount);
+        rows = Mathf.CeilToInt(sqareRoot);
+        columns = Mathf.CeilToInt(sqareRoot);
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = parentWidth / (float)columns - ((spacing.x / (float)columns) * 2) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = parentHeight / (float)rows - ((spacing.y / (float)rows) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = cellWidth;
+        cellSize.y = cellHeight;
+
+        int columnCount = 0;
+        int rowCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+
+    public override void SetLayoutVertical()
+    {
+        // TODO: Implement vertical layout logic here
+    }
+}
---
+++ GameManagerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public int target;
+    public List<int> permutations;
+    public int gridWidth = 2;
+    public int gridHeight = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateTargetAndPermutations();
+    }
+
+    void GenerateTargetAndPermutations()
+    {
+        target = Random.Range(4, 100);
+        permutations = new List<int>();
+        for (int i = 0; i < gridHeight; i++)
+        {
+            int randomNum = Random.Range(1, target);
+            while (permutations.Contains(randomNum))
+            {
+                randomNum = Random.Range(1, target);
+            }
+            permutations.Add(randomNum);
+            permutations.Add(target - randomNum);
+        }
+        Debug.Log("Target: " + target);
+        Debug.Log("Permutations: " + string.Join(", ", permutations));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
