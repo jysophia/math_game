5fed0b61ede48fd5870e1de59ee27e7e361917f9
jysophia <jysophiakim@gmail.com>
2025-09-25 18:40:04 -0400

    able to print numberPrefab value when clicked

--- TestingInputSystem.cs
+++ TestingInputSystem.cs
@@ -1,40 +1,15 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
-public class TestingInputSystem : MonoBehaviour
+public class TestingInputSystem : MonoBehaviour, IPointerClickHandler
 {
     private GameObject numberPrefab;
-    private PlayerInput playerInput;
 
-    private void PlayerInput_onActionTriggered(InputAction.CallbackContext obj)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
-    }
-    
-    public void Click(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            PointerEventData pointerData = new PointerEventData(EventSystem.current)
-            {
-                position = mousePos
-            };
-
-            var raycastResult = new System.Collections.Generic.List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointerData, raycastResult);
-
-            foreach (var result in raycastResult)
-            {
-                if (result.gameObject.GetComponentInChildren<NumberCellScript>())
-                {
-                    numberPrefab = result.gameObject;
-                    Debug.Log("Clicked on: " + numberPrefab.GetComponent<TextMeshProUGUI>().text);
-                }
-            }
-        }
-
+        numberPrefab = eventData.pointerCurrentRaycast.gameObject;
+        var text = numberPrefab.GetComponentInChildren<TextMeshProUGUI>().text;
+        Debug.Log("Clicked on " + text);
     }
 }
